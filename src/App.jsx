7f719a54f503f9f95322142a9520e9296deb4e28
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2023-02-22 21:14:58 +0000

    render header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { GlobalStyle } from "./styles"
+import Header from './components/Header'
 function App() {
 
   return (
     <div className="App">
       <GlobalStyle />
-      <h1>asdkjfsa</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import {Logo, Header} from './styles'
+function HeaderComponent() {
+  return (
+    <div>
+      <Header>
+        <Logo title="Milky Mood Logo" />
+      </Header>
+    </div>
+  )
+}
+
+export default HeaderComponent
