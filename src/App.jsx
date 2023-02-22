6056fdddd7c3c6391e0cec3053e39488791d3d52
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2023-02-22 20:50:23 +0000

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { GlobalStyle } from "./styles"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>asdkjfsa</h1>
     </div>
   )
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  color: #FBFBFB;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  background: #2828e6;
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 500;
+}
+`
