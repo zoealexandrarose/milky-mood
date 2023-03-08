85d6e9de28dd6c279a7a2e4353f418a135b71677
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2023-03-08 14:02:06 +0000

    form ternary state

--- src/components/ModalInner/index.jsx
+++ src/components/ModalInner/index.jsx
@@ -7,14 +7,20 @@ function ModalInner() {
 
   return (
     <Container>
+
+    {showSuccess ? (
+       <>
+       <h4>Yay! We're so excited!</h4>
+       <p>We hope you are too. In the meantime you can follow us on social media to keep up to date:</p>
+     </>
+    ) : (
+     <>
       <h3>Sign Up</h3>
       <p>Join our newsletter to be notified of when pre-release tickets for our first ever world tour go on sale.</p>
       <Form />
+     </>
+    )}
 
-      <>
-        <h4>Yay! We're so excited!</h4>
-        <p>We hope you are too.</p>
-      </>
     </Container>
   )
 }
